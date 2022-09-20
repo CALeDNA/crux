592456251465bc08b1fca2c0bbebe83b2dd6a38d
hector-baez <hbaez@oxy.edu>
2022-09-19 21:45:59 -0500

    removing lines from test run

--- crux/app/ecopcr/run_ecopcr.sh
+++ crux/app/ecopcr/run_ecopcr.sh
@@ -21,15 +21,14 @@ source ${CONFIG}
 
 # conda activate base;
 
-# HOSTNAME=$(hostname | tr -dc '0-9')
-HOSTNAME="19"
+HOSTNAME=$(hostname | tr -dc '0-9')
 OUTPUT="fasta_output"
 
 # download link files
 gocmd get -c ${CYVERSE} /iplant/home/shared/eDNA_Explorer/urls/${RUNID}/chunk${HOSTNAME}/ .
 
 # run obi_ecopcr.sh on every links file
-find chunk${HOSTNAME}/* | parallel -I% --tag --max-args 1 -P ${THREADS} ./obi_ecopcr.sh -g % -p ${PRIMERS} -o ${OUTPUT} -b % -e 3 -s 100 -l 10000 >> logs 2>&1
+find chunk${HOSTNAME}/* | parallel -I% --tag --max-args 1 -P ${THREADS} ./obi_ecopcr.sh -g % -p ${PRIMERS} -o ${OUTPUT} -b % -e ${ERROR} -s ${MINLENGTH} -l ${MAXLENGTH} -c ${CONFIG} >> logs 2>&1
 
 gocmd -c ${CYVERSE} mkdir /iplant/home/shared/eDNA_Explorer/ecopcr/${RUNID}
 gocmd -c ${CYVERSE} mkdir /iplant/home/shared/eDNA_Explorer/ecopcr/logs/${RUNID}
