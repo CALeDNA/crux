833cd6cfa4681dc35575bfffe737028aa1d220a5
hector-baez <hbaez@oxy.edu>
2022-09-21 19:04:04 -0500

    changed folder format

--- crux/app/taxfilter/get-largest.sh
+++ crux/app/taxfilter/get-largest.sh
@@ -13,7 +13,7 @@ done
 source ${CONFIG}
 
 # download nucl acc2taxid
-wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz
+wget -q -c --tries=0 ftp://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz
 gunzip nucl_gb.accession2taxid.gz
 
 START=$(( $HOSTNAME * 2 + 2))
@@ -37,7 +37,8 @@ do
     do
         chunk=$(printf '%02d' "$i")
         # download one bam file at a time
-        wget -P ${SAMDIR}/${primer} https://data.cyverse.org/dav-anon/iplant/projects/eDNA_Explorer/bwa-output/${primer}-nt${chunk}.fasta.bam
+        echo "wget -q -c --tries=0 -P ${SAMDIR}/${primer} https://data.cyverse.org/dav-anon/iplant/home/shared/eDNA_Explorer/bwa/bwa-output/${RUNID}/${primer}-nt${chunk}.fasta.bam"
+        wget -q -c --tries=0 -P ${SAMDIR}/${primer} https://data.cyverse.org/dav-anon/iplant/home/shared/eDNA_Explorer/bwa/bwa-output/${RUNID}/${primer}-nt${chunk}.fasta.bam
         # convert to sam
         samtools view -o ${SAMDIR}/${primer}/${primer}-nt${chunk}.fasta.sam ${SAMDIR}/${primer}/${primer}-nt${chunk}.fasta.bam
         # remove bam
@@ -49,8 +50,9 @@ do
     done
 
     # upload to cyverse
+    gocmd -c ${CYVERSE} mkdir /iplant/home/shared/eDNA_Explorer/fa-taxid/${RUNID}/
     gocmd -c ${CYVERSE} mkdir /iplant/home/shared/eDNA_Explorer/fa-taxid/${RUNID}/${primer}
-    gocmd -c ${CYVERSE} mkdir /iplant/home/shared/eDNA_Explorer/fa-taxid/${RUNID}/logs
+    gocmd -c ${CYVERSE} mkdir /iplant/home/shared/eDNA_Explorer/fa-taxid/logs/${RUNID}
     
     gocmd -c ${CYVERSE} put ${SAMDIR}/${primer}/chunk${HOSTNAME}.fa /iplant/home/shared/eDNA_Explorer/fa-taxid/${RUNID}/${primer}/chunk${HOSTNAME}.fa
     gocmd -c ${CYVERSE} put logs.txt /iplant/home/shared/eDNA_Explorer/fa-taxid/${RUNID}/logs/logs_${HOSTNAME}.fa
