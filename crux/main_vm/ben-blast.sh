b76c9f979d724c8ee7f55a3c0ef358758728b9dc
hector-baez <hbaez@oxy.edu>
2023-04-17 18:57:11 -0500

    move ben dependencies to Dockerfile

--- crux/main_vm/ben-blast.sh
+++ crux/main_vm/ben-blast.sh
@@ -30,10 +30,10 @@ for line in ${PRIMERS}
 do
     for (( nt=0; nt<$NTOTAL; nt++ ))
     do
-        $((nt=10#$nt))
-        nt=$(printf '%02d' $nt)
+        nt_=$((10#$nt))
+        nt_=$(printf '%02d' $nt_)
         primer=$( echo $line | cut -d ',' -f3 )
-        job=$primer-$nt-blast-$RUNID
-        ben add -c "cd crux; docker run -t -v ~/crux/crux/app/blast:/mnt -v ~/crux/crux/vars:/vars -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION --name $job crux /mnt/blast.sh -c "/vars/crux_vars.sh" -j $job -i $RUNID -p $primer -n $nt" $job -o $OUTPUT
+        job=$primer-$nt_-blast-$RUNID
+        ben add -c "cd crux; docker run -t -v ~/crux/crux/app/blast:/mnt -v ~/crux/crux/vars:/vars -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION --name $job crux /mnt/blast.sh -c "/vars/crux_vars.sh" -j $job -i $RUNID -p $primer -n $nt_" $job -o $OUTPUT
     done
 done
