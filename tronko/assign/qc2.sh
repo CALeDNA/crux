aadfd09d9dd131bff680805a657d6ef35f86b102
Hector Baez <hbaez@oxy.edu>
2023-06-13 11:12:48 -0500

    qc/tronko cleanup

--- tronko/assign/qc2.sh
+++ tronko/assign/qc2.sh
@@ -28,15 +28,15 @@ git clone -b cruxv2 https://github.com/CALeDNA/Anacapa.git
 
 
 # EDIT THESE
-BASEDIR="~/Anacapa"
+BASEDIR="./Anacapa"
 DB="$BASEDIR/Anacapa_db"
-DATA="~/$PROJECTID-$PRIMER/samples"
-OUT="~/$PROJECTID-$PRIMER/${PROJECTID}QC"
+DATA="./$PROJECTID-$PRIMER/samples"
+OUT="./$PROJECTID-$PRIMER/${PROJECTID}QC"
 
 # OPTIONAL
-FORWARD="~/$PROJECTID-$PRIMER/forward_primers.txt"
-REVERSE="~/$PROJECTID-$PRIMER/reverse_primers.txt"
-LENGTH="~/$PROJECTID-$PRIMER/metabarcode_loci_min_merge_length.txt"
+FORWARD="./$PROJECTID-$PRIMER/forward_primers.txt"
+REVERSE="./$PROJECTID-$PRIMER/reverse_primers.txt"
+LENGTH="./$PROJECTID-$PRIMER/metabarcode_loci_min_merge_length.txt"
 # FORWARD="/home/ubuntu/crux/tronko/assign/$PROJECTID-$PRIMER/forward_primers.txt"
 # REVERSE="/home/ubuntu/crux/tronko/assign/$PROJECTID-$PRIMER/reverse_primers.txt"
 # LENGTH="/home/ubuntu/crux/tronko/assign/$PROJECTID-$PRIMER/metabarcode_loci_min_merge_length.txt"
@@ -48,45 +48,39 @@ mv tmp "$FORWARD"
 grep -A 1 ">$PRIMER" "$REVERSE" > tmp
 mv tmp "$REVERSE"
 
-
-cd $BASEDIR || exit
-
 time $DB/anacapa_QC_dada2.sh -i $DATA -o $OUT -d $DB -f $FORWARD -r $REVERSE -m 50 -q 30
 
-cd
-
 # upload $OUT
-aws s3 sync $PROJECTID-$PRIMER/${PROJECTID}QC/$PRIMER/paired/filtered s3://ednaexplorer/projects/$PROJECTID/QC/OUT/$PRIMER/paired --no-progress --endpoint-url https://js2.jetstream-cloud.org:8001/
-aws s3 sync $PROJECTID-$PRIMER/${PROJECTID}QC/$PRIMER/unpaired_F/filtered s3://ednaexplorer/projects/$PROJECTID/QC/OUT/$PRIMER/unpaired_F --no-progress --endpoint-url https://js2.jetstream-cloud.org:8001/
-aws s3 sync $PROJECTID-$PRIMER/${PROJECTID}QC/$PRIMER/unpaired_R/filtered s3://ednaexplorer/projects/$PROJECTID/QC/OUT/$PRIMER/unpaired_R --no-progress --endpoint-url https://js2.jetstream-cloud.org:8001/
+aws s3 sync $PROJECTID-$PRIMER/${PROJECTID}QC/$PRIMER/${PRIMER}_sort_by_read_type/paired/filtered s3://ednaexplorer/projects/$PROJECTID/QC/$PRIMER/paired --no-progress --endpoint-url https://js2.jetstream-cloud.org:8001/
+aws s3 sync $PROJECTID-$PRIMER/${PROJECTID}QC/$PRIMER/${PRIMER}_sort_by_read_type/unpaired_F/filtered s3://ednaexplorer/projects/$PROJECTID/QC/$PRIMER/unpaired_F --no-progress --endpoint-url https://js2.jetstream-cloud.org:8001/
+aws s3 sync $PROJECTID-$PRIMER/${PROJECTID}QC/$PRIMER/${PRIMER}_sort_by_read_type/unpaired_R/filtered s3://ednaexplorer/projects/$PROJECTID/QC/$PRIMER/unpaired_R --no-progress --endpoint-url https://js2.jetstream-cloud.org:8001/
 
 # upload QC logs
-aws s3 sync $PROJECTID-$PRIMER/${PROJECTID}QC/Run_info s3://ednaexplorer/projects/$PROJECTID/QC/Run_info --no-progress --endpoint-url https://js2.jetstream-cloud.org:8001/
+aws s3 sync $PROJECTID-$PRIMER/${PROJECTID}QC/Run_info s3://ednaexplorer/projects/$PROJECTID/QC/$PRIMER/Run_info --no-progress --endpoint-url https://js2.jetstream-cloud.org:8001/
 
 
 # add ben tronko-assign jobs
 # run tronko assign paired/unpaired_F/R on $PRIMER and sample file
-cd $PROJECTID-$PRIMER/${PROJECTID}QC/$PRIMER/unpaired_F/filtered || exit
-find . -type f -name '*_F_filt.fastq*' | sed 's/\.\///g' | sed 's/_F_filt\.fastq.*//g' | while read -r filename; do
-    if [[ -e "../../paired/filtered/${filename}_F_filt.fastq" ]]; then
+cd $PROJECTID-$PRIMER/${PROJECTID}QC/$PRIMER/${PRIMER}_sort_by_read_type/unpaired_F/filtered || exit
+find . -type f -name '*_F_filt.fastq.gz' | sed 's/\.\///g' | sed 's/_F_filt\.fastq.gz//g' | while read -r filename; do
+    if [[ -e "../paired/${filename}_F_filt.fastq.gz" ]]; then
         parameters="-1 -2 -3"
     else
         parameters="-2"
-    fi
+    fi;
     
-    ben add -s $BENSERVER -c "cd crux; docker run --rm -t -v ~/crux/tronko/assign:/mnt -v ~/crux/crux/vars:/vars -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION --name $PROJECTID-assign-$filename crux /mnt/assign.sh -f $filename -i $PROJECTID -p $PRIMER $parameters" $PROJECTID-assign-$filename -f main -o $OUTPUT
+    ben add -s /tmp/ben-assign -c "cd crux; docker run --rm -t -v /home/ubuntu/crux/tronko/assign:/mnt -v /home/ubuntu/crux/crux/vars:/vars -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION --name $PROJECTID-$PRIMER-assign-$filename crux /mnt/assign.sh -f $filename -i $PROJECTID -p $PRIMER $parameters" $PROJECTID-$PRIMER-assign-$filename -o /etc/ben/output;
 done
 
 # add unpaired_R files missed
 cd ../../unpaired_R/filtered || exit
-find . -type f -name '*_R_filt.fastq*' | sed 's/\.\///g' | sed 's/_R_filt\.fastq.*//g' | while read -r filename; do
-    if [[ ! -e "../../paired/filtered/${filename}_R_filt.fastq" ]]; then
-        ben add -s $BENSERVER -c "cd crux; docker run --rm -t -v ~/crux/tronko/assign:/mnt -v ~/crux/crux/vars:/vars -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION --name $PROJECTID-assign-$filename crux /mnt/assign.sh -f $filename -i $PROJECTID -p $PRIMER -3" $PROJECTID-assign-$filename -f main -o $OUTPUT
+find . -type f -name '*_R_filt.fastq.gz' | sed 's/\.\///g' | sed 's/_R_filt\.fastq.gz//g' | while read -r filename; do
+    if [[ ! -e "../paired/${filename}_R_filt.fastq.gz" ]]; then
+        ben add -s /tmp/ben-assign -c "cd crux; docker run --rm -t -v /home/ubuntu/crux/tronko/assign:/mnt -v /home/ubuntu/crux/crux/vars:/vars -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION --name $PROJECTID-$PRIMER-assign-$filename crux /mnt/assign.sh -f $filename -i $PROJECTID -p $PRIMER -3" $PROJECTID-$PRIMER-assign-$filename -o /etc/ben/output;
     else
-        echo "Skipping $filename - already in queue."
+        echo "Skipping $filename - already in queue.";   
     fi
 done
-
 
 # clean up
 rm -r $PROJECTID-$PRIMER
