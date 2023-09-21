889688ad6befef6eb939ec04ad888242aa39eef1
hector-baez <hbaez@oxy.edu>
2023-09-21 18:54:34 -0500

    add /tmp to docker

--- crux/app/ecopcr/ecopcr.sh
+++ crux/app/ecopcr/ecopcr.sh
@@ -52,7 +52,7 @@ rm $PRIMER-$LINKS.fasta
 # add ben blast job for each NT chunk
 for ((nt=0; nt<$NTOTAL; nt++)); do
     nt=$(printf '%03d' $nt)
-    ben add -s $BENSERVER -c "cd crux; docker run --rm -t -v ~/crux/crux/app/blast:/mnt -v ~/crux/crux/vars:/vars -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION --name $PRIMER-blast-$LINKS-$nt-$RUNID crux /mnt/blast.sh -c /vars/crux_vars.sh -j $PRIMER-blast-$LINKS-$nt-$RUNID -i $RUNID -p $PRIMER -n $nt -e $LINKS.fasta" $PRIMER-blast-$LINKS-$nt-$RUNID -o /etc/ben/output
+    ben add -s $BENSERVER -c "cd crux; docker run --rm -t -v ~/crux/crux/app/blast:/mnt -v ~/crux/crux/vars:/vars -v /tmp:/tmp -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION --name $PRIMER-blast-$LINKS-$nt-$RUNID crux /mnt/blast.sh -c /vars/crux_vars.sh -j $PRIMER-blast-$LINKS-$nt-$RUNID -i $RUNID -p $PRIMER -n $nt -e $LINKS.fasta" $PRIMER-blast-$LINKS-$nt-$RUNID -o /etc/ben/output
     nt=$(echo $nt | sed 's/^0*//')  # Remove leading zeros
 done
 
