81222c6cb059f98f219032396292009de60a564b
hector-baez <hbaez@oxy.edu>
2023-09-28 10:56:35 -0500

    fix args

--- tronko/assign/qc.sh
+++ tronko/assign/qc.sh
@@ -4,7 +4,7 @@ export AWS_MAX_ATTEMPTS=3
 
 OUTPUT="/etc/ben/output"
 RUNID="2023-04-07"
-while getopts "i:p:b:a:k:s:r:" opt; do
+while getopts "i:p:b:a:k:s:r:K:S:R:B:" opt; do
     case $opt in
         i) PROJECTID="$OPTARG"
         ;;
